cdd533f8e9a43b2164414c9f037491431ef717d8
Colton Plumley <cplumley1025@gmail.com>
2023-05-21 17:12:04 -0500

    Initial commit

---
+++ lib/todo_ex_web/components/layouts.ex
@@ -0,0 +1,5 @@
+defmodule TodoExWeb.Layouts do
+  use TodoExWeb, :html
+
+  embed_templates "layouts/*"
+end
---
+++ lib/todo_ex_web/controllers/page_html.ex
@@ -0,0 +1,5 @@
+defmodule TodoExWeb.PageHTML do
+  use TodoExWeb, :html
+
+  embed_templates "page_html/*"
+end
