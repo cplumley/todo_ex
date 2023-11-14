d82c0ffc683104e84885e1306fd6fa187171162a
Colton Plumley <cplumley1025@gmail.com>
2023-11-14 10:14:49 -0600

    feat: sort incomplete and completed tasks

--- lib/todo_ex_web/live/task_live/task_component.ex
+++ lib/todo_ex_web/live/task_live/task_component.ex
@@ -6,32 +6,58 @@ defmodule TodoExWeb.TaskLive.TaskComponent do
   @impl true
   def render(assigns) do
     ~H"""
-    <div id={"tasks-#{@project_id}"} phx-update="stream" class="flex flex-col gap-2">
-      <div :for={{id, task} <- @streams.tasks} id={id}>
-        <Atoms.card horizontal={true} disabled={task.completed}>
-          <button phx-click={JS.push("toggle_completed", target: @myself, value: %{id: task.id})}>
-            <Atoms.todo_checkbox checked={task.completed} />
-          </button>
-          <div>
-            <p><%= task.title %></p>
-            <p class="text-xs"><%= task.description %></p>
-          </div>
-        </Atoms.card>
+    <div class="flex flex-col gap-2">
+      <div id={"incomplete-tasks-#{@project_id}"} phx-update="stream" class="flex flex-col gap-2">
+        <div :for={{id, task} <- @streams.incomplete_tasks} id={id}>
+          <.task_card task={task} myself={@myself} />
+        </div>
+      </div>
+      <div id={"completed-tasks-#{@project_id}"} phx-update="stream" class="flex flex-col gap-2">
+        <div :for={{id, task} <- @streams.completed_tasks} id={id}>
+          <.task_card task={task} myself={@myself} />
+        </div>
       </div>
     </div>
     """
   end
 
+  def task_card(assigns) do
+    ~H"""
+    <Atoms.card horizontal={true} disabled={@task.completed}>
+      <button phx-click={JS.push("toggle_completed", target: @myself, value: %{id: @task.id})}>
+        <Atoms.todo_checkbox checked={@task.completed} />
+      </button>
+      <div>
+        <p><%= @task.title %></p>
+        <p class="text-xs"><%= @task.description %></p>
+      </div>
+    </Atoms.card>
+    """
+  end
+
   @impl true
-  def update(%{task: task}, socket) do
-    {:ok, stream_insert(socket, :tasks, task)}
+
+  def update(%{task: %{completed: true} = task}, socket) do
+    {:ok,
+     stream_delete(socket, :incomplete_tasks, task)
+     |> stream_insert(:completed_tasks, task)}
+  end
+
+  def update(%{task: %{completed: false} = task}, socket) do
+    {:ok,
+     stream_delete(socket, :completed_tasks, task)
+     |> stream_insert(:incomplete_tasks, task)}
   end
 
   def update(%{project: project}, socket) do
+    incomplete_tasks = Enum.filter(project.tasks, fn t -> not t.completed end)
+    completed_tasks = Enum.filter(project.tasks, fn t -> t.completed end)
+
     {:ok,
      socket
      |> assign(project_id: project.id)
-     |> stream(:tasks, project.tasks)}
+     |> stream(:incomplete_tasks, incomplete_tasks)
+     |> stream(:completed_tasks, completed_tasks)}
   end
 
   @impl true
@@ -39,11 +65,6 @@ defmodule TodoExWeb.TaskLive.TaskComponent do
     task = TaskManager.get_task!(id)
     {:ok, _task} = TaskManager.toggle_completed(task)
 
-    {:noreply, socket}
-  end
-
-  def handle_info(%{task: task}, socket) do
-    send_update(TodoExWeb.TaskLive.TaskComponent, id: task.project_id, task: task)
     {:noreply, socket}
   end
 end
